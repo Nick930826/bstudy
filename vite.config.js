2607321aa648534036478dfb9ff4bc3273d225ae
Nick930826 <xianyou1993@qq.com>
2022-02-10 11:55:12 +0800

    udpate

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/bstudy',  // 对应仓库的项目名称
+  build: {
+    outDir: 'docs',
+  },
   plugins: [vue()]
 })

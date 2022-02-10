706dbb628cdc0d91f986f84de455ec3ca4bcb6c2
Nick930826 <xianyou1993@qq.com>
2022-02-10 11:57:48 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bstudy',  // 对应仓库的项目名称
+  base: '/bstudy',  // 对应仓库的项目名称，
   build: {
     outDir: 'docs',
   },

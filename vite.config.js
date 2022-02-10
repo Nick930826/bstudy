cc61979e9c4839bbc636f2613638f566d950f1f7
Nick930826 <xianyou1993@qq.com>
2022-02-10 12:00:38 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bstudy',  // 对应仓库的项目名称，
+  base: '/bstudy',  // 对应仓库的项目名称，打包完后，构建目录下的index.html静态资源的引入方式，前缀会多出 /assets  -> /bstudy/assets
   build: {
-    outDir: 'docs',
+    outDir: 'docs', // github中的gh-pages分支，支持的文档文件夹名称就是 docs。
   },
   plugins: [vue()]
 })
